36ef3b0f9e6d20035f91f8223edecb2d827b0e7e
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2025-05-30 13:42:45 +0700

    Refactor: move note model, use env URI

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
+
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -15,30 +18,14 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -97,7 +84,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
